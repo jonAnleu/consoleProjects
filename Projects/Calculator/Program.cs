07ae52e21d04df1648a00d78241cfe35571957be
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-07-09 04:35:44 -0500

    calculator

---
+++ Projects/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿int num1 = 0; 
+int num2 = 0;
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number:");
+num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
