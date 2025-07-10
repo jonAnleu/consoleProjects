b0263d2de4d621b116852c6df9794433751a7a68
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-07-10 09:24:52 -0500

    simple password checker

---
+++ Projects/FCC/Program.cs
@@ -0,0 +1,11 @@
+﻿/* SIMPLE PASSWORD CHECKER
+Console.Write("Enter a password: ");
+string password = Console.ReadLine();
+Console.Write("Enter password again to confirm: ");
+string confirm = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(confirm))
+{
+  string result = (password == confirm)? "Passwords Match": "Passwords don't match";
+  Console.Write(result);
+} */
