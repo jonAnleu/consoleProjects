a0c2cd1ff62604a74dd4ea6291a4753e98b09b6e
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-07-11 08:24:11 -0500

    todo initial

--- Projects/FCC/Program.cs
+++ Projects/FCC/Program.cs
@@ -9,3 +9,24 @@ if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(confirm))
   string result = (password == confirm)? "Passwords Match": "Passwords don't match";
   Console.Write(result);
 } */
+
+List<string> words = new List<string>() {"ONE", "TWO", "ONE", "three", "four"};
+
+ List<string> GetOnlyUpperCaseWords(List<string> words)
+{
+  List<string> upperCaseWords = new List<string>();
+
+  foreach (string word in words)
+  {
+    // Check if all characters in the word are uppercase letters
+    if (word.All(char.IsUpper))
+    {
+      upperCaseWords.Add(word);
+    }
+  }
+  Console.WriteLine(upperCaseWords);
+  return upperCaseWords;
+
+  
+}
+  GetOnlyUpperCaseWords(words);
